89af4b09329aee54afbea2953072a790af388792
shubham <shubham.meena751@gmail.com>
2023-12-21 17:30:58 +0530

    login page complete

--- myapp/src/components.js/Header.js
+++ myapp/src/components.js/Header.js
@@ -1,11 +1,46 @@
-import React from 'react'
+import React from "react";
+import { signOut } from "firebase/auth";
+import { auth } from "./utils/Firebase";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+  const navigate = useNavigate();
+  const user = useSelector((store) => store.user);
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+        navigate("/");
+      })
+      .catch((error) => {
+        // An error happened.
+        navigate("/error");
+      });
+  };
   return (
-    <div className='absolute px-8 py-2 bg-gradient-to-b from-black z-10' >
-    <img className='w-44' src='https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png' alt='photo'/>
+    <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
+      <img
+        className="w-[7rem]"
+        src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+        alt="photo"
+      />
+      {user && <div className="flex p-2  ">
+        <img
+          className="w-8 h-8 rounded-[4px] mt-1"
+          src={user.photoURL}alt="usericon"
+        />
+        <img
+          className="w-8 h-8 rounded-[4px] mt-1"
+          src="https://occ-0-4409-3647.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABTZ2zlLdBVC05fsd2YQAR43J6vB1NAUBOOrxt7oaFATxMhtdzlNZ846H3D8TZzooe2-FT853YVYs8p001KVFYopWi4D4NXM.png?r=229"
+          alt="usericon"
+        />
+        <button className="p-2 text-white font-medium" onClick={handleSignOut}>
+          Sign Out
+        </button>
+      </div>}
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- myapp/src/components.js/utils/Validate.js
+++ myapp/src/components.js/utils/Validate.js
@@ -1,9 +1,9 @@
-export const checkValidData =(name,email,password)=>{
+export const checkValidData =(email,password)=>{
 const isEmailValid = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(email)
 const isPassvalid = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password)
-const isNamevalid =/^[a-zA-Z\\s]*$/.test(name)
+// const isNamevalid =/^[a-zA-Z\\s]*$/.test(name)
 
-if(!isNamevalid) return "Name id Not valid"
+// if(!isNamevalid) return "Name id Not valid"
 
 if(!isEmailValid)return "Email Id is not valid"
  
